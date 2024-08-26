c98373f5579d3a18aa4aa5d00e37f208fd0b3dc7
IlluminatisSon <bayram.wilson04@gmail.com>
2024-08-26 09:21:30 +0200

    implemented error handling and more userfriendly

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-# {new_key:new_value for (index, row) in df.iterrows()}
-# {"A": "Alfa", "B": "Bravo"}
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 dict = {row.letter : row.code for (index, row) in df.iterrows()}
-# print(dict)
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-list = [dict[letter] for letter in word if letter in dict]
-print(list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        list = [dict[letter] for letter in word]
+    except KeyError:
+        print("there are illegal values inside")
+        generate_phonetic()
+    else:
+        print(list)
+generate_phonetic()
